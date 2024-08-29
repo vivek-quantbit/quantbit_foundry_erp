3cd596a39875a534768889c2e35c298a06d9f368
vivek-quantbit <vivek.kumbhar@erpdata.in>
2024-08-29 10:11:27 +0530

    aug291010viv

--- quantbit_foundry_erp/hooks.py
+++ quantbit_foundry_erp/hooks.py
@@ -227,3 +227,11 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    {
+        "doctype": "Role",
+        "filters": [
+            ["name", "in", ["Foundry Operator","Foundry Supervisor"]],
+        ],
+    },
+]
