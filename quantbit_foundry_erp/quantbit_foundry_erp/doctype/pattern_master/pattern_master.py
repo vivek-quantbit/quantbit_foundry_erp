c16d8486c6760a9900d46665e6a79af1b4cd04b8
vivek-quantbit <vivek.kumbhar@erpdata.in>
2024-09-05 16:19:32 +0530

    sup050419viv

--- quantbit_foundry_erp/quantbit_foundry_erp/doctype/pattern_master/pattern_master.py
+++ quantbit_foundry_erp/quantbit_foundry_erp/doctype/pattern_master/pattern_master.py
@@ -7,7 +7,7 @@ from frappe.model.document import Document
 
 class PatternMaster(Document):
 	def validate(self):
-		pass
+		self.validation_data()
 
 
 	@frappe.whitelist()
@@ -16,3 +16,14 @@ class PatternMaster(Document):
 		for d in self.get('pattern_master_casting_material_details'):
 			final_listed.append(d.casting_item_code)
 		return final_listed
+
+	def validation_data(self , child_table=None , child_field = None , validate_from_field  = None , validating_attribute = None):
+		for d in self.get("pattern_master_casting_treatment_details"):
+			data = d.get('finished_source_warehouse')
+			validation_data = frappe.get_value("Warehouse", data , "company")
+			# frappe.throw(str(company))
+			if self.company == validation_data:
+				pass
+			else:
+				frappe.throw("frappe the invalida data")
+		
